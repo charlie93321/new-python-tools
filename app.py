6018d4dcf64584514f0f4a46b5539eb7756c45b9
xiaoyu <2459060612@qq.com>
2020-10-12 19:01:23 +0800

    init-i18n

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from random import Random
 import com.dxm.flask_web.i18nLangApp
 from flask import Flask, send_from_directory
 app = Flask(__name__, template_folder="templates", static_folder="static", static_url_path="/static")
+@app.route('/')
+def hello_world():
+      return "hello"
 
+from com.dxm.flask_web.i18nLangApp import *
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5500)
--- com/dxm/flask_web/i18nLangApp.py
+++ com/dxm/flask_web/i18nLangApp.py
@@ -4,6 +4,6 @@ import com.dxm.flask_web.i18nLangApp
 from flask import Flask, send_from_directory
 app = Flask(__name__, template_folder="templates", static_folder="static", static_url_path="/static")
 
-@app.route('/')
-def hello_world():
-      return "hello"
+@app.route('/2')
+def hello_world2():
+      return "hello2"
